eca67467b0f1d58a595333cc64b653864e79c4db
Jessie Wang <jessie.wang@futurefit.ai>
2020-09-06 21:02:23 -0400

    added production config to knexconfig file

--- knexfile.js
+++ knexfile.js
@@ -15,6 +15,15 @@ const config = {
       max: 10
     },
     migrations: {
+      directory: __dirname + '/migrations',
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/migrations',
       tableName: 'knex_migrations'
     }
   }
