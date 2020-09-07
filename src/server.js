bb638ec6f7e61964ed444923ef4e11af62791a74
Jessie Wang <jessie.wang@futurefit.ai>
2020-09-07 12:17:21 -0400

    added morgan middleware for request logging and fixed server msg on listen

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 const app = express();
 
 import auth from './routes/auth.js';
@@ -8,11 +9,18 @@ import nominate from './routes/nominate.js';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
 app.use('/auth', auth);
 app.use('/search', search);
 app.use('/nominate', nominate);
 
-app.listen(process.env.PORT || 5000, () => {
+app.get('/healthcheck', (req, res) => {
+  return res.status(200).json({
+    msg: 'I\'m fine thanks for checking!'
+  })
+})
+
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server listening on port 8000!')
 });
